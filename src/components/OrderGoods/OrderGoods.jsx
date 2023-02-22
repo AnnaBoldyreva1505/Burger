4f2cdbe1844f65bd715a325190a516ba0cea45eb
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-02-22 14:50:59 +0600

    11

---
+++ src/components/OrderGoods/OrderGoods.jsx
@@ -0,0 +1,32 @@
+import { OrderCount } from "../OrderCount/OrderCount";
+import style from "./OrderGoods.module.css";
+
+
+
+export const OrderGoods = () => {
+
+
+
+    return (
+        <li className={style.item}>
+        <img
+          className={style.image}
+          src="../../assets/img/burger_1.jpg"
+          alt="Супер сырный"
+        />
+
+        <div className={style.goods}>
+          <h3 className={style.title}>Супер сырный</h3>
+
+          <p className={style.weight}>512г</p>
+
+          <p className={style.price}>
+            1279
+            <span className={style.currency}>₽</span>
+          </p>
+        </div>
+
+<OrderCount count={11}/>
+      </li>
+    )
+}
