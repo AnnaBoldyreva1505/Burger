f99fd731c39416a08e0a9c527aac76a4b28b06ec
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-02-25 21:11:52 +0600

    1111

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,21 +1,26 @@
 import style from "./Order.module.css";
 import {OrderGoods } from "../OrderGoods/OrderGoods.jsx";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 
-const orderList = [
-  "Суперсырный",
-  "Фри",
-  "Хот-дог",
+const orderList = [ "Суперсырный", "Фри", "Хот-дог",
 ]
 
-
 export const Order = () => {
+const {totalPrice, totalCount} = useSelector(state => state.order)
+const dispatch = useDispatch()
+useEffect(() => {
+  dispatch(order)
+}) 
+
+
   return (
     <div className={style.order}>
       <section className={style.wrapper}>
         <div className={style.header} tabIndex="0" role="button">
           <h2 className={style.order_title}>Корзина</h2>
 
-          <span className={style.count}>4</span>
+          <span className={style.count}>{totalCount}</span>
         </div>
 
         <div className={style.wrap_list}>
@@ -27,7 +32,7 @@ export const Order = () => {
           <div className={style.total}>
             <p>Итого</p>
             <p>
-              <span className={style.amount}>1279</span>
+              <span className={style.amount}>{totalPrice} </span>
               <span className={style.currency}>₽</span>
             </p>
           </div>
